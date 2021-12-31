9c2364dc0e7a87ccfa35be4f12e6b5d2ae5c5d10
sayidimrannazari <Sayidi_n@yahoo.com>
2021-12-30 21:33:23 -0500

    added some new stuff

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
         dryRun = false,
-        tags = "@calculator"
+        tags = "@etsy"
 )
 public class CukesRunner {
 }
--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -4,7 +4,10 @@ import com.cybertek.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
+import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 /**
  * similar to TestBase.java in testNg framework
@@ -14,15 +17,38 @@ import io.cucumber.java.en.Given;
 public class Hooks {
 
     @Before
-    public void setUpScenario(){
+    public void setUpScenario() {
         System.out.println("BEFORE - setUp method is running before the scenario ");
-
     }
 
     @After
-    public void tearDownScenario(){
-        System.out.println("AFTER - tearDown scenario is running after the scenario");
+    public void tearDownScenario() {
 
-        Driver.closeDriver();
+        System.out.println("AFTER - tearDown method is running after the scenario");
+        // Driver.closeDriver();
     }
+    @After
+    public void tearDownScenario(Scenario scenario) {
+        /**
+         * Scenario scenario: represents current running cucumber scenario
+         * -cast webdriver to TakesScreenshot interface.(TakesScreenshot)Driver.getDriver()
+         * -call getScreenShotAs method and output type as OutputType.BYTES
+         * -save the result into byte[] array: byte[] image
+         * -attach the image into the scenario html report: scenario.attach(image, "image/png", scenario.getName());
+         */
+        if (scenario.isFailed()) {
+            byte[] image = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(image, "image/png", scenario.getName());
+        }
+        System.out.println("AFTER - tearDown method is running after the scenario:" + scenario.getName());
+        Driver.closeDriver();
+
+        /**
+         * @AfterStep - runs after each scenario
+         *
+         */
+    }
+
+
+
 }
