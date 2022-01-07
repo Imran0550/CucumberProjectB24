19527db74491798c3ec0926e4535c0e8ca0347b9
sayidimrannazari <Sayidi_n@yahoo.com>
2022-01-06 19:31:07 -0500

    newly added stuff

--- src/test/java/com/cybertek/tests/ExcelRead.java
+++ src/test/java/com/cybertek/tests/ExcelRead.java
@@ -1,11 +1,24 @@
 package com.cybertek.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class ExcelRead {
 
     @Test
-    public void readingFromExcel(){
+    public void readingFromExcel() throws IOException {
+        //Open excel workbook using XSSFWorkBook class
+            XSSFWorkbook workbook = new XSSFWorkbook("Employees.xlsx");
 
+            //Goto worksheet. pass the worksheet name
+        XSSFSheet xssfSheet = workbook.getSheet("data");
+
+        //print mike from the sheet
+        System.out.println(xssfSheet.getRow(1).getCell(0));
+        //print Romanenko from 3rd row
+        System.out.println(xssfSheet.getRow(3).getCell(1));
     }
 }
---
+++ src/test/java/com/cybertek/tests/ExcelReadActions.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests;
+
+import org.junit.Test;
+
+public class ExcelReadActions {
+    String filePath = "";
+
+    @Test
+    public void readExcelSheetData(){
+
+    }
+}
